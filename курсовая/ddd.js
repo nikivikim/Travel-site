5eb5d8e546cf204add35464c86d38e6845a6d0c5
nikivikim <113990436+nikivikim@users.noreply.github.com>
2023-04-23 17:11:18 +0300

    Add files via upload

---
+++ курсовая/ddd.js
@@ -0,0 +1,9 @@
+
+function colorEptaScript(element) {
+var result = "";
+for (var i = 0; i < element.innerHTML.length; i++) {
+result += "<span style='color: rgb(" + ~~(Math.random() * 255) + ", " + ~~(Math.random() * 255) + ", " + ~~(Math.random() * 255) + ")'>" + element.innerHTML.charAt(i) + "</span>"
+}
+element.innerHTML = result;
+}
+
